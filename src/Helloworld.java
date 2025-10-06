50e70693ad0a29c220902e2834baa9be752b1599
Vibhor97-del <vibhorthaokar97@gmail.com>
2025-10-06 10:28:32 +0530

    This commit is merge example

---
+++ src/Helloworld.java
@@ -0,0 +1,8 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("This is my first github commit integreted with jenkins");
+        System.out.println("This commit from ");
+        System.out.println("This is merge example");
+
+    }
+}
